29f7be7925ef9f8cb964cf3a88d08cf82479c5e4
Jay Two Birds <jay@twobirdshealing.org>
2026-04-21 09:05:41 -0500

    add: empty sidebar.php to tbc-starter-theme
    Satisfies WP 3.0+ requirement for the file to exist even though the theme does not use a sidebar.

---
+++ companion plugins/tbc-starter-theme/sidebar.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * The sidebar template.
+ *
+ * This theme does not use a sidebar, but WordPress requires
+ * the file to exist to avoid a deprecation notice (since WP 3.0).
+ *
+ * @package TBC_Starter_Theme
+ */
